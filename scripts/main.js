ebaeb7b160ff67e36cfd1ab127ab69715a9fa768
aleem aheer <aleemaheer5769644@gmail.com>
2022-06-27 18:31:48 +0500

    Added a guessing game

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,23 @@ myImage.onclick = () =>  {
       myImage.setAttribute('src','images/firefox-icon.jpeg');
     }
 }
+
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');
+
+// function to set greeting
+function setUserName() {
+    let myName = prompt("Enter your name: ");
+    if (!myName) setUserName();
+    localStorage.setItem('name', myName);
+    myHeading.textContent = 'Mozilla is cool, ' + myName;
+}
+
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function () { setUserName() }
