edfd5a4bcb677f5c261cd18976ad8d9873763872
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-05 08:58:42 +0500

    Doing some oop concepts

---
+++ beginner/oop/tutorial/main.js
@@ -0,0 +1,10 @@
+const circle = {
+    radius: 1, // radius and location is a property and draw is a function
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function() {
+        console.log('draw');
+    }
+}
