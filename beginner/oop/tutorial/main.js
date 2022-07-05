d48ea3f69471669e5519387f9549fb591249a4b5
aleem aheer <aleemaheer5769644@gmail.com>
2022-07-05 10:56:11 +0500

    tutorial done

--- beginner/oop/tutorial/main.js
+++ beginner/oop/tutorial/main.js
@@ -1,3 +1,4 @@
+/*
 const circle = {
     radius: 1, // radius and location is a property and draw is a function
     location: {
@@ -8,3 +9,52 @@ const circle = {
         console.log('draw');
     }
 }
+
+circle.draw();
+
+// Using factories
+function createCircle(radius) {
+    return {
+        radius,
+        draw: function () {
+            console.log('draw');
+        }
+    }
+}
+const circle = createCircle(1);
+circle.draw();
+*/
+// By constructor function
+// constructor function name will be always capitalized
+function Circle(radius) {
+    this.radius = radius;
+    let defaultLocation = { x: 1, y: 2};
+    this.getDefaultLocation = function () {
+        return defaultLocation;
+    }
+    this.draw = function () {
+        console.log('draw');
+    }
+}
+
+const circle = new Circle(1);
+circle.location = { x: 1};
+circle.radius = 10;
+// We can also delete some properties, for example when sending to other users, we will delete the sensitive information
+delete circle.location;
+
+for (let key in circle) {
+    if (typeof circle[key] !== 'function')
+        console.log(key, circle[key]);
+}
+
+const keys = Object.keys(circle);
+console.log(keys);
+for (key of keys) {
+    console.log(key);
+}
+
+if ('radius' in circle) {
+    console.log("Circle has radius");
+}
+console.log(circle.getDefaultLocation());
